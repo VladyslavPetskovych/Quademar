c214e1e2629c8e56b7e4d8f4ead3105d212e9070
vladyslav <peckovychv@gmail.com>
2026-03-10 16:27:43 +0200

    test

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
           ГВАРДАМАР
         </h1>
         <p className="text-zinc-600 dark:text-zinc-400">
-          ІСТОРІЯ ПОЧИНАЄТЬСЯ ТУТ
+          ІСТОРІЯ ПОЧИНАЄТЬСЯ ТУТ!!!
         </p>
       </main>
     </div>
